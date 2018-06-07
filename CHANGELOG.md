49088f1fa565f65999a28f4bbe5f35c6ca0708ad
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-06-07 17:56:19 +0200

    0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## 0.4.0
+
+#### New Features
+
+  - Add route function for testing #11
+
+
 ## 0.3.0
 
 #### New Features
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct.module.bidi "0.3.0"
+(defproject duct.module.bidi "0.4.0"
   :description "Duct module and router for the Bidi routing library"
   :url "https://github.com/druids/duct.module.bidi"
   :license {:name "MIT License"
